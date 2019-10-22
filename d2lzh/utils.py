5b20a52880e680fc327a8a6cf3ed0e56854c8bc8
Zhang Hao <zhanghao@rock-chips.com>
2019-10-22 11:33:03 +0800

    complete chapter 6.3

--- d2lzh/utils.py
+++ d2lzh/utils.py
@@ -69,3 +69,37 @@ def metric_accuracy(y_true, y_pred):
     ypred = K.cast(K.argmax(y_pred, axis=-1), K.floatx())
     acc = K.equal(ytrue, ypred)
     return K.mean(acc)
+
+
+def data_iter_random(corpus_indices, batch_size, num_steps, ctx=None):
+    # 减1是因为输出的索引是相应输入的索引加1
+    num_examples = (len(corpus_indices) - 1) // num_steps
+    epoch_size = num_examples // batch_size
+    example_indices = list(range(num_examples))
+    random.shuffle(example_indices)
+
+    # 返回从pos开始的长为num_steps的序列
+    def _data(pos):
+        return corpus_indices[pos: pos + num_steps]
+
+    for i in range(epoch_size):
+        # 每次读取batch_size个随机样本
+        i = i * batch_size
+        batch_indices = example_indices[i: i + batch_size]
+        X = [_data(j * num_steps) for j in batch_indices]
+        Y = [_data(j * num_steps + 1) for j in batch_indices]
+        yield np.array(X, ctx), np.array(Y, ctx)
+
+
+def data_iter_consecutive(corpus_indices, batch_size, num_steps):
+    corpus_indices = np.array(corpus_indices)
+    data_len = len(corpus_indices)
+    batch_len = data_len // batch_size
+    indices = corpus_indices[0: batch_size*batch_len].reshape((
+        batch_size, batch_len))
+    epoch_size = (batch_len - 1) // num_steps
+    for i in range(epoch_size):
+        i = i * num_steps
+        X = indices[:, i: i + num_steps]
+        Y = indices[:, i + 1: i + num_steps + 1]
+        yield X, Y

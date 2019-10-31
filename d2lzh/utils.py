f4559eb9e6d8da022ed2ba4970f8ef3019271598
Zhang Hao <zhanghao@rock-chips.com>
2019-10-31 22:17:07 +0800

    add chapter 7.2 7.4 7.5

--- d2lzh/utils.py
+++ d2lzh/utils.py
@@ -157,3 +157,27 @@ def train_and_predict_rnn_gluon(model, vocab_size, corpus_indices, idx_to_char, 
                     prefix, pred_len, model, vocab_size, idx_to_char,
                     char_to_idx, num_steps))
 
+
+def train_2d(trainer):  # 本函数将保存在d2lzh包中方便以后使用
+    x1, x2, s1, s2 = -5, -2, 0, 0  # s1和s2是自变量状态，本章后续几节会使用
+    results = [(x1, x2)]
+    for i in range(20):
+        x1, x2, s1, s2 = trainer(x1, x2, s1, s2)
+        results.append((x1, x2))
+    print('epoch %d, x1 %f, x2 %f' % (i + 1, x1, x2))
+    return results
+
+
+def show_trace_2d(f, results):  # 本函数将保存在d2lzh包中方便以后使用
+    plt.plot(*zip(*results), '-o', color='#ff7f0e')
+    x1, x2 = np.meshgrid(np.arange(-5.5, 1.0, 0.1), np.arange(-3.0, 1.0, 0.1))
+    plt.contour(x1, x2, f(x1, x2), colors='#1f77b4')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+
+
+def get_data_ch7():  # 本函数已保存在d2lzh包中方便以后使用
+    data = np.genfromtxt('../data/airfoil_self_noise.dat', delimiter='\t')
+    data = (data - data.mean(axis=0)) / data.std(axis=0)
+    return np.array(data[:1500, :-1]), np.array(data[:1500, -1])
+
